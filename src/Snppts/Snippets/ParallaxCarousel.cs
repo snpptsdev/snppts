874004ba7295c6fbf79bc9b9f775733185d67d55
sthewissen <github@thewissen.io>
2019-06-04 09:44:41 +0200

    Initial samples

---
+++ src/Snppts/Snippets/ParallaxCarousel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class ParallaxCarousel : IAmASnippet
+    {
+        public string Slug => "parallax-carousel";
+        public string Title => "Parallax Carousel";
+        public string GithubRepoName => "sthewissen/KickassUI.ParallaxCarousel";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "A Xamarin.Forms version of a parallax carousel to prove you can create goodlooking UI with Xamarin.Forms as talked about in my blogpost on: <a href=\"https://www.thewissen.io/creating-a-parallax-carousel-in-xamarin-forms\" target=\"_blank\">https://www.thewissen.io/creating-a-parallax-carousel-in-xamarin-forms</a>";
+
+        public IAmAnAuthor AuthorInfo => new StevenThewissen();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri(),
+            new Uri(),
+            new Uri(),
+            new Uri()
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.WALKTHROUGHS, Category.CARDS
+        };
+    }
+}
---
+++ src/Snppts/Snippets/Traveler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class Traveler : IAmASnippet
+    {
+        public string Slug => "traveler";
+        public string Title => "Traveler";
+        public string GithubRepoName => "sthewissen/KickassUI.Traveler";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "A Xamarin.Forms version of the Traveler app to prove you can create goodlooking UI with Xamarin.Forms as talked about in my blogpost on: <a href=\"https://www.thewissen.io/travel-broadens-the-mind/\" target=\"_blank\">https://www.thewissen.io/travel-broadens-the-mind/</a>";
+
+        public IAmAnAuthor AuthorInfo => new StevenThewissen();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/sthewissen/KickassUI.Traveler/master/droid.jpg"),
+            new Uri("https://raw.githubusercontent.com/sthewissen/KickassUI.Traveler/master/ios.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.CARDS, Category.LISTS, Category.DASHBOARD
+        };
+    }
+}
