e56a718aea796e7b5c88ee87571bc39ac0d15e15
sthewissen <github@thewissen.io>
2019-06-04 22:27:02 +0200

    Added Charlin's samples from the previous version

---
+++ src/Snppts/Snippets/IntroductionPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class IntroductionPage : IAmASnippet
+    {
+        public string Slug => "introduction-page";
+        public string Title => "Introduction Page";
+        public string GithubRepoName => "CrossGeeks/Xamarin.Samples";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "Simple and beautiful introduction Page. More Info: <a href=\"https://xamgirl.com/cool-introduction-page-using-xamarin-forms\" target=\"_blank\">https://xamgirl.com/cool-introduction-page-using-xamarin-forms</a>";
+
+        public IAmAnAuthor AuthorInfo => new CharlinAgramonte();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/snpptsdev/MigratedImages/master/61/108.jpg"),
+            new Uri("https://raw.githubusercontent.com/snpptsdev/MigratedImages/master/61/109.jpg")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LAUNCHSCREEN
+        };
+    }
+}
---
+++ src/Snppts/Snippets/LoginWithImageEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class LoginWithImageEntry : IAmASnippet
+    {
+        public string Slug => "login-with-imageentry";
+        public string Title => "Login with ImageEntry";
+        public string GithubRepoName => "CrossGeeks/Xamarin.Forms.UI";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "Login UI. NOTE: Check I'm using a custom renderer, the implementation is in the Android/iOS project. <a href=\"https://xamgirl.com/image-entry-in-xamarin-forms\" target=\"_blank\">https://xamgirl.com/image-entry-in-xamarin-forms</a>.";
+
+        public IAmAnAuthor AuthorInfo => new CharlinAgramonte();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/snpptsdev/MigratedImages/master/68/114.jpg")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LOGIN
+        };
+    }
+}
