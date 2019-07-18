e9359b5e3e9efddb581ab2d862e590780d839592
roubachof <jm.alfonsi@gmail.com>
2019-07-18 17:35:18 +0200

    add jean-marie alfonsi as author and run away! as new UI snippet

---
+++ src/Snppts/Authors/JeanMarieAlfonsi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Snppts.Infrastructure;
+
+namespace Snppts.Authors
+{
+    public class JeanMarieAlfonsi : IAmAnAuthor
+    {
+        public string FirstName => "Jean-Marie";
+        public string LastName => "Alfonsi";
+        public Uri Website => new Uri("https://www.sharpnado.com");
+        public string TwitterHandle => "piskariov";
+        public string GitHubHandle => "roubachof";
+        public string GravatarHash => "793803979d0ef8d047e83c86fd2ad67f";
+    }
+}
