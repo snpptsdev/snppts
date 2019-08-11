e875e1dff9290c56aa3f6fe59cf87d0056d0f119
Kym Phillpotts <kphillpotts@gmail.com>
2019-08-11 21:56:07 +1000

    Added Day Vs Night Design

---
+++ src/Snppts/Snippets/DayVsNight.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class DayVsNight : IAmASnippet
+    {
+        public string Slug => "day-vs-night";
+        public string Title => "Day Vs Night";
+        public string GithubRepoName => "kphillpotts/DayVsNight";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "Gradients, so many gradients. Skia controls. User selectable themeing.";
+
+        public IAmAnAuthor AuthorInfo => new KymPhillpotts();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://kymphillpotts.com/assets/images/posts/dayvsnight/day.png"),
+            new Uri("https://kymphillpotts.com/assets/images/posts/dayvsnight/night.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.WIDGETS, Category.PROFILES, Category.DASHBOARD
+        };
+
+        public Uri ExternalUri => new Uri("https://kymphillpotts.com/xamarin-forms-ui-challenge-dayvsnight.html");
+    }
+}
