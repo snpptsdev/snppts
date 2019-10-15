49612c398c37951b2812520e19fd6a47615a0576
Ione Souza Junior <junior@ionixjunior.com.br>
2019-10-15 13:12:43 -0300

    Update author information

--- src/Snppts/Authors/IoneSouzaJunior.cs
+++ src/Snppts/Authors/IoneSouzaJunior.cs
@@ -7,9 +7,10 @@ namespace Snppts.Authors
     {
         public string FirstName => "Ione";
         public string LastName => "Souza Junior";
-        public Uri Website => null;
-        public string TwitterHandle => "";
+        public Uri Website => new Uri("https://www.ionixjunior.com.br");
+        public string TwitterHandle => "ionixjunior";
         public string GitHubHandle => "ionixjunior";
+        public string EmailAddress => "junior@ionixjunior.com.br";
         public string GravatarHash => "ca2812c8e2e51b07c5185663d1c3f033";
     }
 }
