a38819c1252046631af7044bc14c2b5e2a3432ba
egbakou <egbakoukodjolaurent@gmail.com>
2019-11-06 16:33:37 +0000

    Added Credit-Card-Checkout snippet.

---
+++ src/Snppts/Snippets/CreditCardCheckout.cs
@@ -0,0 +1,34 @@
+﻿using Snppts.Authors;
+using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Snppts.Snippets
+{
+    public class CreditCardCheckout : IAmASnippet
+    {
+        public string Slug => "credit-card-checkout";
+        public string Title => "Credit Card Checkout";
+        public string GithubRepoName => "egbakou/CreditCardCheckout";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "A Xamarin.Forms UI Challenge based on a fantastic dribble design.";
+
+        public IAmAnAuthor AuthorInfo => new KodjoLaurentEgbakou();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/egbakou/CreditCardCheckout/master/design/Screenshot_20191105-112705.png"),
+            new Uri("https://raw.githubusercontent.com/egbakou/CreditCardCheckout/master/design/Screenshot_20191105-112636.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.CARDS, Category.TIMELINE, Category.SHOPPINGCART
+        };
+
+        public Uri ExternalUri => new Uri("https://lioncoding.com/2019/11/05/2019-11-05-Credit-Card-checkout-CarouselView-de-Xamarin-forms-4-3-en-action/");
+    }
+}
