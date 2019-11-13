e82c0f7bb3473333b7bde389c4f4ea3b8da76cf3
Kym Phillpotts <kphillpotts@gmail.com>
2019-11-13 21:32:54 +1100

    Added MarvelCards

---
+++ src/Snppts/Snippets/MarvelCards.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class MarvelCards : IAmASnippet
+    {
+        public string Slug => "marvel-cards";
+        public string Title => "Marvel Cards";
+        public string GithubRepoName => "kphillpotts/MarvelCards";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "A swiping card UI with loads of animations with Skia Sharp.";
+
+        public IAmAnAuthor AuthorInfo => new KymPhillpotts();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/kphillpotts/MarvelCards/master/design/Screenshot_1565521630.png"),
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.CARDS, Category.LISTS, Category.POPOVERS, Category.NAVIGATIONS, 
+        };
+
+        public Uri ExternalUri => null;
+    }
+}
