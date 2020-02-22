84387c3a1706c85438ea505c740338883c10e574
Logesh Palani <logesh.01@hotmail.com>
2020-02-22 13:48:03 +0530

    Login and Signup Snippet added

---
+++ src/Snppts/Snippets/LoginAndSignUp.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class LoginAndSignUp : IAmASnippet
+    {
+        public string Slug => "Login and Sign Up";
+        public string Title => "Login and Sign Up Page";
+        public string GithubRepoName => "logeshpalani98/Xamarin_UI";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "A simple and clean good looking login and sign up screen";
+
+        public IAmAnAuthor AuthorInfo => new LogeshPalani();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/logeshpalani98/Xamarin_UI/master/XamarinForms_SignupLogin/Screenshot/IMG_0754.jpg"),
+            new Uri("https://raw.githubusercontent.com/logeshpalani98/Xamarin_UI/master/XamarinForms_SignupLogin/Screenshot/IMG_0755.jpg")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.Login, Category.CARDS
+        };
+
+        public Uri ExternalUri => new Uri("https://github.com/logeshpalani98/Xamarin_UI");
+    }
+}
