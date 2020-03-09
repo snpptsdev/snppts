cc226defa95e6e700c667912bd91e2edd1302d1f
Alexandre Lima <alexandreslima@outlook.com>
2020-03-09 15:31:50 -0300

    add mine subscription plan

---
+++ src/Snppts/Authors/AlexandreSanlim.cs
@@ -0,0 +1,23 @@
+﻿using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snppts.Authors
+{
+    public class AlexandreSanlim : IAmAnAuthor
+    {
+        public string FirstName => "Alexandre";
+
+        public string LastName => "Sanlim";
+
+        public Uri Website => null;
+
+        public string TwitterHandle => "alexandresanlim";
+
+        public string GitHubHandle => "alexandresanlim";
+
+        public string GravatarHash => "201e2428b23bfe48fbb3e02e54f6652d";
+    }
+}
