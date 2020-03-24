043ef96016d1675f23ca30b42b1c08931a94bc7d
Marc-Anthony <marclatty1@gmail.com>
2020-03-24 01:50:46 -0500

    Add Covid19-tracker snppt

---
+++ src/Snppts/Snippets/Covid19TrackerUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class Covid19TrackerUI : IAmASnippet
+    {
+        public string Slug => "covid19-tracker-ui";
+
+        public string Title => "Covid19 Tracker App";
+
+        public string Description => "Covid19.Forms is a tracker app to keep updated with Covid19";
+
+        public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("AnthonyLatty/Covid19.Forms");
+
+        public Uri ExternalUri => null;
+
+        public bool ContainsAndroidSample => true;
+
+        public bool ContainsiOSSample => true;
+
+        public bool ContainsUWPSample => false;
+
+        public IAmAnAuthor AuthorInfo => new MarcAnthonyLatty();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_droid_3.jpg"),
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_droid_1.jpg"),
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_droid_2.jpg"),
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_ios_3.png"),
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_ios_1.png"),
+            new Uri("https://github.com/AnthonyLatty/Covid19.Forms/blob/master/Artwork/artwork_ios_2.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.CARDS,
+            Category.FRIENDS
+        };
+    }
+}
