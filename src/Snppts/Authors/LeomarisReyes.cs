f22d62cf08ad3cfa040af91738cfe3307fd33f51
Leomaris Reyes <reyes.leomaris@gmail.com>
2020-03-29 23:12:13 -0400

    Author added

---
+++ src/Snppts/Authors/LeomarisReyes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Snppts.Infrastructure;
+
+namespace Snppts.Authors
+{
+    public class LeomarisReyes : IAmAnAuthor
+    {
+        public string FirstName => "Leomaris";
+        public string LastName => "Reyes";
+        public Uri Website => new Uri("https://AskXammy.com/");
+        public string TwitterHandle => "leomarisreyes11";
+        public string GitHubHandle => "leomarisreyes";
+        public string EmailAddress => "lreyes@stemelle.com";
+        public string GravatarHash => "ae78e84a683611c7b72c9ba829c125e0";
+    }
+}
---
+++ src/Snppts/Snippets/FaceBookApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class FaceBookApp : IAmASnippet
+    {
+        public string Slug => "FacebookApp";
+        public string Title => "Facebook App";
+        public string GithubRepoName => "leomarisreyes/FaceBookUISample";
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "It's the replica of Facebook App.";
+
+        public IAmAnAuthor AuthorInfo => new LeomarisReyes();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://askxammy.com/wp-content/uploads/2019/08/FB_post.png"),
+            new Uri("https://askxammy.com/wp-content/uploads/2019/10/AndroidFB.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.APPCLONE , Category.ARTICLES
+        };
+
+
+        public Uri ExternalUri => new Uri("https://askxammy.com/replicating-facebook-profile-ui-in-xamarin-forms/");
+
+        public GitHubRepoInfo GitHubRepoInfo => throw new NotImplementedException();
+    }
+
+}
