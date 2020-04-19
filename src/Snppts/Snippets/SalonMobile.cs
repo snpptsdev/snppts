d74433ee6719c71cf9c8bd0c1442a4527e635dbe
Kainã Fontoura Almeida <kainafa@hotmail.com>
2020-04-19 11:40:59 -0300

    Add Author and Screenshots of SalonMobile

---
+++ src/Snppts/Snippets/SalonMobile.cs
@@ -0,0 +1,36 @@
+﻿using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Snppts.Snippets
+{
+    public class SalonMobile : IAmASnippet
+    {
+        public string Slug => "salon-mobile";
+        public string Title => "Salon Mobile";
+        public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("kainaalmeida/SalonMobile");
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "This snippet was created to demonstrate some animations";
+
+        public IAmAnAuthor AuthorInfo => new KainaAlmeida();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://github.com/kainaalmeida/SalonMobile/blob/master/screenshots/iOS01.png"),
+            new Uri("https://github.com/kainaalmeida/SalonMobile/blob/master/screenshots/iOS02.png")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LISTS,
+            Category.PHOTOS
+        };
+
+        public Uri ExternalUri => null;
+    }
+}
