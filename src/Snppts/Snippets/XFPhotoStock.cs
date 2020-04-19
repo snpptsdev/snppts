3e76bceb512b36ebcc4a98b1f444da6e7338facd
Ufuk <ufukzimmerman@gmail.com>
2020-04-20 00:01:06 +0300

    Add Xamarin Forms Photo Stock UI

---
+++ src/Snppts/Snippets/XFPhotoStock.cs
@@ -0,0 +1,38 @@
+using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Snppts.Snippets
+{
+    public class PhotoStockUIDesign : IAmASnippet
+    {
+        public string Slug => "XamarinForms-PhotoStock-UI-Design";
+        public string Title => "Xamarin Forms PhotoStock UI Design";
+        public string Description => "Xamarin Forms photo stock application design.";
+        public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("ufukhawk/XamarinForms-PhotoStock-UI-Design");
+        public Uri ExternalUri => null;
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+        public IAmAnAuthor AuthorInfo => new AltevirCardosoNeto();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/ufukhawk/Xamarin-Forms-UI-Library/master/Images/PhotoStock.png"),
+            new Uri("https://raw.githubusercontent.com/ufukhawk/Xamarin-Forms-UI-Library/master/Images/PhotoStocksc2.png"),
+            new Uri("https://raw.githubusercontent.com/ufukhawk/Xamarin-Forms-UI-Library/master/Images/PhotoStock.gif"),
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LISTS,
+            Category.PHOTOS,
+            Category.SEARCH,
+            Category.DASHBOARD,
+            Category.SETTINGS,
+            Category.FILTER
+        };
+    }
+}
