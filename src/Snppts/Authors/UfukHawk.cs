8e07693351ae7fabe38963faf68110c66212c2d4
Ufuk <ufukzimmerman@gmail.com>
2020-04-20 09:23:46 +0300

    Add UfukHawk.cs (#81)
    * Add UfukHawk.cs
    
    * Update Link
    
    * Edit

---
+++ src/Snppts/Authors/UfukHawk.cs
@@ -0,0 +1,12 @@
+using System;
+using Snppts.Infrastructure;
+public class UfukHawk : IAmAnAuthor
+{
+    public string FirstName => "Ufuk";
+    public string LastName => "Sahin";
+    public Uri Website => new Uri("https://www.youtube.com/channel/UCZPFYjd1wVjmr2fDuKNRXHA");
+    public string TwitterHandle => "ufukhawk";
+    public string GitHubHandle => "ufukhawk";
+    public string EmailAddress => "ufukzimmerman@gmail.com";
+    public string GravatarHash => "04caa6410c1627f2ed3359a1a380266f";
+}
