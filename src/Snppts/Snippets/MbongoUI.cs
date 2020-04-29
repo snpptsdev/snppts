8adb5aa17ff64d42767c56891b9314cfb39d3e79
Amaro Manungu <amaro.manungu@tistech.co.ao>
2020-04-29 09:25:50 +0100

    MbongoUIApp

--- src/Snppts/Snippets/MbongoUI.cs
+++ src/Snppts/Snippets/MbongoUI.cs
@@ -25,11 +25,9 @@ namespace Snppts.Snippets
         };
 
         public IList<Category> Categories => new List<Category>
-        {
-                     
-			
+        {                    			
             Category.WIDGETS,
-			Category.LAUNCHSCREEN
+			Category.LAUNCHSCREEN,
             Category.LISTS
         };
     }
