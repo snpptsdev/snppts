4479621f406bb76da025764871e23c5fc3aba212
Vicente Guzman <luciomsp@geeks.ms>
2020-06-15 10:34:07 -0500

    Add Carousel Intro Page

---
+++ src/Snppts/Authors/VicenteGuzman.cs
@@ -0,0 +1,16 @@
+using System;
+using Snppts.Infrastructure;
+
+namespace Snppts.Authors
+{
+    public class RendyDelRosario : IAmAnAuthor
+    {
+        public string FirstName => "Vicente";
+        public string LastName => "Guzmán";
+        public Uri Website => new Uri("https://vicenteguzman.mx/");
+        public string TwitterHandle => "luciomsp";
+        public string GitHubHandle => "luciomsp";
+        public string EmailAddress => "luciomsp@geeks.ms";
+        public string GravatarHash => "72cce778aac0d6066a14225e90c30874";
+    }
+}
---
+++ src/Snppts/Snippets/CarouselIntroPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+
+namespace Snppts.Snippets
+{
+    public class CarouselIntroPage : IAmASnippet
+    {
+        public string Slug => "carousel-intro-page";
+        public string Title => "Carousel Intro Page";
+
+        public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("LucioMSP/CarouselIntroPage");
+
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+
+        public string Description => "Simple example of an introductory page in Xamarin.Forms.";
+
+        public IAmAnAuthor AuthorInfo => new VicenteGuzman();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/luciomsp/CarouselIntroPage/master/ScreenShots/ScreenShot01.png"),
+            new Uri("https://raw.githubusercontent.com/luciomsp/CarouselIntroPage/master/103/ScreenShot02.png"),
+            new Uri("https://raw.githubusercontent.com/luciomsp/CarouselIntroPage/master/103/ScreenShot03.png"),
+        }
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LOGIN
+        };
+
+        public Uri ExternalUri => null;
+    }
+}
