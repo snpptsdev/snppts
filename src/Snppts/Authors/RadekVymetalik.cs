98a6898bb2a5b2266b7fa576c8f597d11872ebe1
Radek Vymětalík <rvymetalik@email.cz>
2020-10-16 20:56:20 +0200

    Added GadgetsStoreApp

---
+++ src/Snppts/Authors/RadekVymetalik.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Snppts.Infrastructure;
+
+namespace Snppts.Authors
+{
+    public class RadekVymetalik : IAmAnAuthor
+    {
+        public string FirstName => "Radek";
+        public string LastName => "Vymětalík";
+        public Uri Website => new Uri("https://github.com/RadekVyM");
+        public string TwitterHandle => "";
+        public string GitHubHandle => "RadekVyM";
+        public string EmailAddress => "rvymetalik@email.cz";
+        public string GravatarHash => "";
+    }
+}
