2951acc5345352c8290febbf557e61a03d6a11b2
Radek Vymětalík <rvymetalik@email.cz>
2021-01-26 18:57:04 +0100

    Added BetApp and CallingApp.

---
+++ src/Snppts/Snippets/CallingApp.cs
@@ -0,0 +1,34 @@
+﻿using Snppts.Authors;
+using Snppts.Extensions;
+using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Snppts.Snippets
+{
+    public class CallingApp : IAmASnippet
+    {
+        public string Slug => "calling-app";
+        public string Title => "Calling App";
+        public string Description => "A Xamarin.Forms application based on majority wallet & calling design by Hampus Öhman from Dribbble.com.";
+        public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("RadekVyM/Calling-App");
+        public Uri ExternalUri => null;
+        public bool ContainsAndroidSample => true;
+        public bool ContainsiOSSample => true;
+        public bool ContainsUWPSample => false;
+        public IAmAnAuthor AuthorInfo => new RadekVymetalik();
+
+        public IEnumerable<Uri> ImageUris => new List<Uri>
+        {
+            new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/callingapp.gif"),
+            new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/callingapp2.gif")
+        };
+
+        public IList<Category> Categories => new List<Category>
+        {
+            Category.LISTS,
+            Category.CARDS,
+            Category.NAVIGATIONS
+        };
+    }
+}
