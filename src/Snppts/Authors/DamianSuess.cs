0bc1760c78c3246d0b0b65ad10989f1ddf61b786
Damian Suess <djsuess@gmail.com>
2022-08-10 10:35:26 -0400

    Added MAUI Credit Card Payment UI using Prism.Maui

---
+++ src/Snppts/Authors/DamianSuess.cs
@@ -0,0 +1,19 @@
+﻿using Snppts.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snppts.Authors
+{
+    public class DamianSuess : IAmAnAuthor
+    {
+        public string FirstName => "Damian";
+        public string LastName => "Suess";
+        public Uri Website => new Uri("https://suesslabs.com");
+        public string TwitterHandle => "";
+        public string GitHubHandle => "DamianSuess";
+        public string EmailAddress => "noreply@email.com";
+        public string GravatarHash => "5e39caab2d1ae3cf43efb99a2bfd4d4b";
+    }
+}
