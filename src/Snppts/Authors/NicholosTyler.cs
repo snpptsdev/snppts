74b526dd3c1bef63c54731e6d03c24be5566ed10
Nicholos Tyler <nicholos.tyler@outlook.com>
2022-08-12 14:42:39 -0400

    Added MAUI Health Tracker UI

---
+++ src/Snppts/Authors/NicholosTyler.cs
@@ -0,0 +1,12 @@
+using System;
+using Snppts.Infrastructure;
+
+public class NicholosTyler : IAmAnAuthor
+{
+    public string FirstName => "Nicholos";
+    public string LastName => "Tyler";
+    public Uri Website => new Uri("https://github.com/nicholostyler");
+    public string TwitterHandle => "nicholostyler";
+    public string GitHubHandle => "nicholostyler";
+    public string GravatarHash => "c5f6b23c331e02229190bd023436339e";
+}
