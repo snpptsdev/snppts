40897e25e82f9ee74695977cf1ba1d5649498bde
Satta Sundar <sundarsatta@outlook.com>
2022-08-17 22:49:17 +0530

    Update iOS and MacCatalyst image uri

--- src/Snppts/Snippets/StarbucksRedisgn.cs
+++ src/Snppts/Snippets/StarbucksRedisgn.cs
@@ -12,7 +12,7 @@ namespace Snppts.Snippets
         public string Title => "Maui Starbuck Redesign";
         public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("sattasundar/maui-starbucks-ui");
         public bool ContainsAndroidSample => true;
-        public bool ContainsiOSSample => false;
+        public bool ContainsiOSSample => true;
         public bool ContainsUWPSample => false;
 
         public string Description => "StarbucksRedesign App UI build in all new .NET MAUI ";
@@ -24,6 +24,10 @@ namespace Snppts.Snippets
             new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/android_home.png"),
             new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/android_detail.png"),
             new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/android_splash.png"),
+            new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/ios_home.png"),
+            new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/ios_detail.png"),
+            new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/mac_home.png"),
+            new Uri("https://raw.githubusercontent.com/sattasundar/maui-starbucks-ui/main/images/mac_detail.png"),
         };
 
         public IList<Category> Categories => new List<Category>
