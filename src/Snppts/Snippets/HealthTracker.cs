e8039d01ad2cf2750782ec21a507a61380c87059
Gerald Versluis <gerald@verslu.is>
2022-08-17 08:07:04 +0000

    Fix URL

--- src/Snppts/Snippets/HealthTracker.cs
+++ src/Snppts/Snippets/HealthTracker.cs
@@ -29,7 +29,7 @@ public class HealthTracker : IAmASnippet
         Category.DASHBOARD,
     };
 
-    public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("https://github.com/nicholostyler/Health-Tracker");
+    public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("nicholostyler/Health-Tracker");
 
     public Uri ExternalUri => new Uri("https://github.com/nicholostyler/Health-Tracker");
 }
