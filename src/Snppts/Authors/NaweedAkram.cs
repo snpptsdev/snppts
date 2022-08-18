855ab859ea5aac8185d8536aa9f9791687cb743b
Naweed Akram <naweed@xgeno.com>
2022-08-18 17:45:26 +0300

    Added naweed-maui-apps

---
+++ src/Snppts/Authors/NaweedAkram.cs
@@ -0,0 +1,11 @@
+﻿namespace Snppts.Authors;
+
+public class NaweedAkram : IAmAnAuthor
+{
+    public string FirstName => "Naweed";
+    public string LastName => "Akram";
+    public Uri Website => new Uri("https://blogs.xgenoapps.com/");
+    public string TwitterHandle => "xgeno";
+    public string GitHubHandle => "naweed";
+    public string GravatarHash => "46335c2a2d6b939d3fb66e71f542eaab";
+}
