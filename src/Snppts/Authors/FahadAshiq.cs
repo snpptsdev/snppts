715a43fdc6347d871d3b68c0706663d5cbf93138
Fahad Ashiq ⚡ <44393684+fahadashiq12@users.noreply.github.com>
2022-08-21 17:43:19 +0500

    Create FahadAshiq.cs

---
+++ src/Snppts/Authors/FahadAshiq.cs
@@ -0,0 +1,10 @@
+public class StevenThewissen : IAmAnAuthor
+{
+    public string FirstName => "Fahad";
+    public string LastName => "Ashiq";
+    public Uri Website => new Uri("https://www.thewissen.io");
+    public string TwitterHandle => "https://twitter.com/iamfahadashiq";
+    public string GitHubHandle => "https://github.com/fahadashiq12";
+    public string EmailAddress => "fahadashiq30@gmail.com";
+    public string GravatarHash => "62ce0e69389e31fd3c42fb230f9b1637";
+}
