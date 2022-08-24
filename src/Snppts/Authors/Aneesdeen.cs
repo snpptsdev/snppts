0c70d238a7c731ad06c060e34100f7446aadea0e
Aneesdeen Abdulzabar <aneesdeen.abdulzabar@mindtree.com>
2022-08-25 00:32:33 +0530

    Cloned version of classic 2048 game build using MAUI.

---
+++ src/Snppts/Authors/Aneesdeen.cs
@@ -0,0 +1,13 @@
+﻿namespace Snppts.Authors;
+
+public class Aneesdeen : IAmAnAuthor
+{
+    public string FirstName => "Anees";
+    public string LastName => "Deen";
+    public Uri Website => new Uri("https://github.com/codeanees");
+    public string TwitterHandle => "tweet2anees";
+    public string GitHubHandle => "codeanees";
+    public string EmailAddress => "aneesdeen91@gmail.com";
+    public string GravatarHash => "19fa5b926ddc9d5c814e1eee6a5161ab";
+}
+
