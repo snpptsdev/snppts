88d3089bb8262d59770582c063c919db3ec22c6e
Santosh Dahal <santoshdahal@bajratechnologies.com>
2022-08-25 20:26:55 +0545

    Added ecommerce and ISP app design sample

---
+++ src/Snppts/Authors/SantoshDahal.cs
@@ -0,0 +1,12 @@
+﻿namespace Snppts.Authors;
+
+public class SantoshDahal : IAmAnAuthor
+{
+    public string FirstName => "Santosh";
+    public string LastName => "Dahal";
+    public Uri Website => new Uri("https://dahalsantosh.com.np");
+    public string TwitterHandle => "exendahal";
+    public string GitHubHandle => "exendahal";
+    public string EmailAddress => "contact@dahalsantosh.com.np";
+    public string GravatarHash => "";
+}
