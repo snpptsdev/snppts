e3c64cd78840043b3ee94599c59386966761f218
AizenDong <abdoulayedong@gmail.com>
2022-08-30 12:13:52 +0100

    Add WeatherXam snippet

---
+++ src/Snppts/Authors/AbdoulayeDong.cs
@@ -0,0 +1,12 @@
+﻿namespace Snppts.Authors
+{
+    public class AbdoulayeDong : IAmAnAuthor
+    {
+        public string FirstName => "Abdoulaye";
+        public string LastName => "Dong";
+        public Uri Website => null;
+        public string TwitterHandle => "AbdoulayeDong";
+        public string GitHubHandle => "AizenDong";
+        public string GravatarHash => "b587892e4a08e593dff65a014337e8ee";
+    }
+}
