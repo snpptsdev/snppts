c42bec732a0ab8aca334cad7f1be33073923feff
Anmol Behl <behl1anmol@gmail.com>
2022-08-31 23:34:46 +0530

    Submission for .NET MAUI Beautiful UI Challenge

---
+++ src/Snppts/Snippets/TodoMe.cs
@@ -0,0 +1,38 @@
+﻿namespace Snppts.Snippets;
+
+public class TodoMe : IAmASnippet
+{
+    public string Slug => "TodoMe";
+
+    public string Title => "Todo.me";
+
+    public string Description => "Todo.me is a simple note taking and task tracking app made with .NET MAUI.";
+
+    public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("behl1anmol/Todo.me");
+
+    public Uri ExternalUri => null;
+
+    public bool ContainsAndroidSample => true;
+
+    public bool ContainsiOSSample => false;
+
+    public bool ContainsUWPSample => false;
+
+    public Sdk BuiltWith => Sdk.Maui;
+
+    public IAmAnAuthor AuthorInfo => new AnmolBehl();
+
+    public IEnumerable<Uri> ImageUris => new List<Uri>
+    {
+        new Uri("https://github.com/behl1anmol/Todo.me/blob/master/Screenshots/Maui_app_todo.gif"),
+        new Uri("https://raw.githubusercontent.com/behl1anmol/Todo.me/master/Screenshots/TodoView.jpg"),
+        new Uri("https://raw.githubusercontent.com/behl1anmol/Todo.me/master/Screenshots/TodoDetailsView.jpg")
+    };
+
+    public IList<Category> Categories => new List<Category>
+    {
+        Category.LISTS,
+        Category.NAVIGATIONS,
+        Category.ACTIVITYFEEDS
+    };
+}
