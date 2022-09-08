7b5e50ba5222fcb81648253441ddb4ae5101fab2
RadekVym <rvymetalik@email.cz>
2022-09-08 17:56:56 +0200

    Updated some of my old snippets to MAUI

--- src/Snppts/Snippets/CallingApp.cs
+++ src/Snppts/Snippets/CallingApp.cs
@@ -4,19 +4,22 @@ public class CallingApp : IAmASnippet
 {
     public string Slug => "calling-app";
     public string Title => "Calling App";
-    public string Description => "A Xamarin.Forms application based on majority wallet & calling design by Hampus Öhman from Dribbble.com.";
+    public string Description => ".NET MAUI implementation of majority wallet & calling design by Hampus Öhman from Dribbble.com.";
     public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("RadekVyM/Calling-App");
     public Uri ExternalUri => null;
     public bool ContainsAndroidSample => true;
     public bool ContainsiOSSample => true;
-    public bool ContainsUWPSample => false;
-    public Sdk BuiltWith => Sdk.XamarinForms;
+    public bool ContainsUWPSample => true;
+    public Sdk BuiltWith => Sdk.Maui;
     public IAmAnAuthor AuthorInfo => new RadekVymetalik();
 
     public IEnumerable<Uri> ImageUris => new List<Uri>
     {
-        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/callingapp.gif"),
-        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/callingapp2.gif")
+        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/android_callingapp_call.gif"),
+        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/ios_callingapp_home.png"),
+        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/ios_callingapp_calling.png"),
+        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/ios_callingapp_call.png"),
+        new Uri("https://raw.githubusercontent.com/RadekVyM/Calling-App/main/Images/windows_callingapp.gif"),
     };
 
     public IList<Category> Categories => new List<Category>
