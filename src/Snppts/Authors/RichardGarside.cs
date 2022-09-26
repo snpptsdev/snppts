c9c884f2fefb7b6e46b24ec495693f73acec214d
Richard Garside <code@nogginbox.co.uk>
2022-09-26 07:26:41 +0100

    Add new Author - Richard Garside (nogginbox)

---
+++ src/Snppts/Authors/RichardGarside.cs
@@ -0,0 +1,9 @@
+public class RichardGarside : IAmAnAuthor
+{
+    public string FirstName => "Richard";
+    public string LastName => "Garside";
+    public Uri Website => new Uri("https://www.nogginbox.co.uk/");
+    public string TwitterHandle => "_richatrdg";
+    public string GitHubHandle => "nogginbox";
+    public string GravatarHash => "af63a841553cea52069a553ae84d0628";
+}
