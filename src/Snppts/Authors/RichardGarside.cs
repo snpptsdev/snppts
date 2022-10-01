b47e242e2a4dfca9682735f84681e75d5179b711
Richard Garside <code@nogginbox.co.uk>
2022-10-01 12:03:24 +0100

    Fix twitter handle

--- src/Snppts/Authors/RichardGarside.cs
+++ src/Snppts/Authors/RichardGarside.cs
@@ -3,7 +3,7 @@ public class RichardGarside : IAmAnAuthor
     public string FirstName => "Richard";
     public string LastName => "Garside";
     public Uri Website => new Uri("https://www.nogginbox.co.uk/");
-    public string TwitterHandle => "_richatrdg";
+    public string TwitterHandle => "_richardg";
     public string GitHubHandle => "nogginbox";
     public string GravatarHash => "af63a841553cea52069a553ae84d0628";
 }
