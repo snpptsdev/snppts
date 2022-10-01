b2f501e743deab7c31a866624f288f66c5d0f50e
Richard Garside <code@nogginbox.co.uk>
2022-10-01 22:12:19 +0100

    Fix image for @VladislavAntonyuk

--- src/Snppts/Snippets/MauiBank.cs
+++ src/Snppts/Snippets/MauiBank.cs
@@ -16,8 +16,7 @@ public class MauiBank : IAmASnippet
     public IEnumerable<Uri> ImageUris => new List<Uri>
     {
         //new Uri("https://vladislavantonyuk.sirv.com/vladislavantonyuk/articles/dotnet-maui-bank.svg"),
-        new Uri("https://camo.githubusercontent.com/dfe2ebb45ee5071331ddd836f4048d545d67ec0644eeb0f9d5b2a615abdd0cf5/68747470733a2f2f766c616469736c6176616e746f6e79756b2e736972762e636f6d2f766c616469736c6176616e746f6e79756b2f61727469636c65732f32332f646f746e65742d6d6175692d62616e6b2d6170702e676966")
-
+        new Uri("https://ik.imagekit.io/VladislavAntonyuk/vladislavantonyuk/articles/23/dotnet-maui-bank-app.gif")
     };
 
     public IList<Category> Categories => new List<Category>
