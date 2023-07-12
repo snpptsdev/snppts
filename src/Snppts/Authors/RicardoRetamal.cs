310aae504503063f2faacbf50764672dda4db85a
Ricardo R <rretamal.dev@gmail.com>
2023-07-11 20:59:48 -0400

    Camera App snippet

---
+++ src/Snppts/Authors/RicardoRetamal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Snppts.Authors
+{
+	public class RicardoRetamal : IAmAnAuthor
+    {
+        public string FirstName => "Ricardo";
+        public string LastName => "Retamal";
+        public Uri Website => null;
+        public string TwitterHandle => "rretamal";
+        public string GitHubHandle => "rretamal";
+        public string EmailAddress => "rretamal.dev@live.com";
+        public string GravatarHash => "";
+    }
+}
+
---
+++ src/Snppts/Snippets/CameraApp.cs
@@ -0,0 +1,27 @@
+﻿namespace Snppts.Snippets;
+
+public class CameraApp : IAmASnippet
+{
+    public string Slug => "camera-app";
+    public string Title => "Camera App";
+    public string Description => ".NET MAUI implementation of a Midjourney design for a Camera Viewer app.";
+    public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("rretamal/MidjourneyCameraApp");
+    public Uri ExternalUri => null;
+    public bool ContainsAndroidSample => true;
+    public bool ContainsiOSSample => true;
+    public bool ContainsUWPSample => false;
+    public Sdk BuiltWith => Sdk.Maui;
+    public IAmAnAuthor AuthorInfo => new RicardoRetamal();
+
+    public IEnumerable<Uri> ImageUris => new List<Uri>
+    {
+        new Uri("https://github.com/rretamal/MidjourneyCameraApp/blob/main/MidjourneyCameraApp/Images/1_mYN3aXSpY5v5QnJ_CKfaCw.png"),
+    };
+
+    public IList<Category> Categories => new List<Category>
+    {
+        Category.DASHBOARD,
+        Category.TABBARS,
+        Category.NAVIGATIONS
+    };
+}
