202c2e7d8413d40b65d8e63d96a676a573c7606a
小 林 <jevonsflash@hotmail.com>
2023-08-17 14:49:07 +0800

    Add MatoMusic-GestureUI

---
+++ src/Snppts/Snippets/MatoMusicGestureUI.cs
@@ -0,0 +1,34 @@
+﻿namespace Snppts.Snippets;
+
+public class MatoMusicGestureUI : IAmASnippet
+{
+    public string Slug => "mato-music-gesture-ui";
+
+    public string Title => "MatoMusic-GestureUI";
+
+    public string Description => "a music player with gesture UI";
+
+    public GitHubRepoInfo GitHubRepoInfo => new GitHubRepoInfo("jevonsflash/maui-samples/tree/master/MatoMusic-GestureUI");
+
+    public Uri ExternalUri => new Uri("https://blog.csdn.net/jevonsflash/article/details/130031596");
+
+    public bool ContainsAndroidSample => true;
+
+    public bool ContainsiOSSample => true;
+
+    public bool ContainsUWPSample => false;
+
+    public Sdk BuiltWith => Sdk.Maui;
+
+    public IAmAnAuthor AuthorInfo => new LinXiao();
+
+    public IEnumerable<Uri> ImageUris => new List<Uri>
+    {
+        new Uri("https://github.com/jevonsflash/maui-samples/blob/master/Assets/a1.gif?raw=true"),
+    };
+
+    public IList<Category> Categories => new List<Category>
+    {
+        Category.PLAYBACK,
+    };
+}
