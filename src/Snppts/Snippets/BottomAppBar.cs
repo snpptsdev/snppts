e913b9133f3ca81e88685dc2c486585efdb3db73
Gerald Versluis <gerald@verslu.is>
2024-02-12 08:42:22 +0100

    Update BottomAppBar.cs

--- src/Snppts/Snippets/BottomAppBar.cs
+++ src/Snppts/Snippets/BottomAppBar.cs
@@ -16,8 +16,10 @@ public class BottomAppBar : IAmASnippet
 
     public IEnumerable<Uri> ImageUris => new List<Uri>
     {
-        new Uri("https://camo.githubusercontent.com/46611cb54cec1ef787e32c27248fa1ba6026f93990f64210cfa15696c3c49923/68747470733a2f2f692e696d6775722e636f6d2f5969696b564c502e706e67"),
-        new Uri("https://camo.githubusercontent.com/4812f8a75d90486ddbaedff2c0ca37ada7abe8c0cd6461696e65a1d912cf9263/68747470733a2f2f692e696d6775722e636f6d2f645844673978462e706e67")
+        new Uri("https://camo.githubusercontent.com/29e5cb48156b3dce2b5cd9add27a6deaa90f4bed4557dd77d9408ab2c881de17/68747470733a2f2f692e696d6775722e636f6d2f5a6d5658356f532e706e67"),
+        new Uri("https://camo.githubusercontent.com/4812f8a75d90486ddbaedff2c0ca37ada7abe8c0cd6461696e65a1d912cf9263/68747470733a2f2f692e696d6775722e636f6d2f645844673978462e706e67"),
+        new Uri("https://camo.githubusercontent.com/80ae6e61a98e804c89115a605859d6e5294568e6c2b34dcb91767759630e0417/68747470733a2f2f692e696d6775722e636f6d2f5969696b564c502e706e67"),
+        new Uri("https://camo.githubusercontent.com/5f2ca743283eabd1ab539924d19f294a2f7bea9b78f5f5f348e2eb1612054328/68747470733a2f2f692e696d6775722e636f6d2f4d38536134654e2e706e67")
     };
 
     public IList<Category> Categories => new List<Category>
