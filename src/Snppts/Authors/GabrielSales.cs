d62f32f30ac0ad1945e24cee4756fbe692d653d2
Gabriel Sales <ghe_sales@hotmail.com>
2024-06-18 13:01:40 -0300

    Authenticator added

---
+++ src/Snppts/Authors/GabrielSales.cs
@@ -0,0 +1,16 @@
+﻿namespace Snppts.Authors;
+
+public class GabrielSales : IAmAnAuthor
+{
+    public string FirstName => "Gabriel";
+
+    public string LastName => "Sales";
+
+    public Uri Website => new Uri("www.linkedin.com/in/gabrielsales-");
+
+    public string TwitterHandle => "itsgsales";
+
+    public string GitHubHandle => "salesHgabriel";
+
+    public string GravatarHash => "6d6fb44b051be467e3408a1d714ad5e660a67d4888cfc503a51dd538f96cfcf3";
+}
